e269560c444023e42546caa3534cde997a38bc5a
MishraAbhishek07 <davmishra96@gmail.com>
2024-12-03 15:36:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
 function highlight() {
-    //Write your code here
-
-
+    // Get all <strong> elements
+    var boldWords = document.getElementsByTagName('strong');
+    // Change the color of each <strong> element to green
+    for (var i = 0; i < boldWords.length; i++) {
+        boldWords[i].style.color = 'green';
+    }
 }
 
-
 function return_normal() {
-    //Write your code here
-
-    
+    // Get all <strong> elements
+    var boldWords = document.getElementsByTagName('strong');
+    // Change the color of each <strong> element back to black
+    for (var i = 0; i < boldWords.length; i++) {
+        boldWords[i].style.color = 'black';
+    }
 }
